4ba4d051b5506a80ca2f5d96d52e8b3e14edc6d0
Ashley Smith <ashley.smith@magneticearth.org>
2023-10-24 16:16:28 +0100

    Move book to subdir and continue docker config

---
+++ build-book.sh
@@ -0,0 +1,12 @@
+uid=1000
+gid=100
+subuidSize=$(( $(podman info --format "{{ range .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+subgidSize=$(( $(podman info --format "{{ range .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+podman run -it --rm \
+    -v "${PWD}":/home/jovyan/work --user $uid:$gid \
+    --uidmap $uid:0:1 --uidmap 0:1:$uid --uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
+    --gidmap $gid:0:1 --gidmap 0:1:$gid --gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
+    swarm-quicklooks bash -c " \
+        cd /home/jovyan/work/jupyterbook/ && \
+        ./build-book.sh \
+    "
