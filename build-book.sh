7daa413da4813183163a1d042e9dedb089115b1e
Ashley Smith <ashley.smith@magneticearth.org>
2023-10-24 18:05:23 +0100

    Run from yet-to-publish main branch

--- build-book.sh
+++ build-book.sh
@@ -1,3 +1,5 @@
+IMAGE_NAME="ghcr.io/swarm-disc/swarm_quicklooks"
+IMAGE_TAG="main"
 uid=1000
 gid=100
 subuidSize=$(( $(podman info --format "{{ range .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
@@ -6,7 +8,7 @@ podman run -it --rm \
     -v "${PWD}":/home/jovyan/work --user $uid:$gid \
     --uidmap $uid:0:1 --uidmap 0:1:$uid --uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
     --gidmap $gid:0:1 --gidmap 0:1:$gid --gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
-    swarm-quicklooks bash -c " \
+    $IMAGE_NAME:$IMAGE_TAG bash -c " \
         cd /home/jovyan/work/jupyterbook/ && \
         ./build-book.sh \
     "
--- run-lab.sh
+++ run-lab.sh
@@ -2,6 +2,8 @@
 # The current directory is mounted within /home/jovyan/work
 # We can allow read/write access within the container by mapping the host user to the jovyan user inside the container
 # See https://jupyter-docker-stacks.readthedocs.io/en/latest/using/running.html#using-the-podman-cli
+IMAGE_NAME="ghcr.io/swarm-disc/swarm_quicklooks"
+IMAGE_TAG="main"
 uid=1000
 gid=100
 subuidSize=$(( $(podman info --format "{{ range .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
@@ -10,4 +12,4 @@ podman run -it --rm -p 8888:8888 \
     -v "${PWD}":/home/jovyan/work --user $uid:$gid \
     --uidmap $uid:0:1 --uidmap 0:1:$uid --uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
     --gidmap $gid:0:1 --gidmap 0:1:$gid --gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
-    swarm-quicklooks bash
+    $IMAGE_NAME:$IMAGE_TAG
