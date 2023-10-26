e3a7ed72527d0d4aa1f293009472e1be2d45d952
Ashley Smith <ashley.smith@magneticearth.org>
2023-10-26 14:41:25 +0100

    Fix

--- jupyterbook/build-book.sh
+++ jupyterbook/build-book.sh
@@ -1,2 +1,2 @@
-mamba run -n jupyterbook pytest --nbmake --overwrite notebooks/intro.ipynb --nbmake-kernel=swarmpal-runner
+mamba run -n jupyterbook pytest --nbmake --overwrite notebooks/intro.ipynb notebooks/fac-test.ipynb --nbmake-kernel=swarmpal-runner
 mamba run -n jupyterbook jupyter-book build --config _config-with-nbmake.yml .
